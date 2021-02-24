6395e92c743544708f2aec43bcf2f76f830a5d32
vaibhav <vgion.tech@gmail.com>
2021-02-24 14:53:45 +0530

    init

---
+++ app/src/main/java/com/example/seekbar/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(100);
+        seekBar.setProgress(50);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressValue = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressValue = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Progress: " + progressValue, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
